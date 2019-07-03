460a9112035ef1405b4cdb1e1aa98f0166895ad1
Nekobitlz <jokers.bit@gmail.com>
2019-07-03 19:12:01 +0600

    Made tasks from tutorial

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,21 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        //TODO FIX ME
+        val parts: List<String>? = fullName?.split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        TODO()
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        return  "${firstName?.get(0)} ${lastName?.get(0)}"
+    }
+}

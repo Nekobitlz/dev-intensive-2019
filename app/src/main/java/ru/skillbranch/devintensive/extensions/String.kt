6d137ba788ee7925518082570e0413fd4661aa4d
Nekobitlz <jokers.bit@gmail.com>
2019-07-04 15:44:46 +0600

    "stripHtml()" - done

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -10,3 +10,27 @@ fun String.truncate(number: Int = 16): String {
         result.trim().plus("...")
     else result.plus("...")
 }
+
+fun String.stripHtml(): String {
+    var result = ""
+    var symbols = 0
+
+    while (symbols < this.length) {
+        when (this[symbols]) {
+            '<' -> while (this[symbols] != '>') { symbols++ }
+            '&' -> {/* DON'T INSERT */}
+            '>' -> {/* DON'T INSERT */}
+            '\'' -> {/* DON'T INSERT */}
+            ' ' -> {
+                result += this[symbols]
+                while (this[symbols] == ' ') { symbols++ }
+                result += this[symbols]
+            }
+            else -> result += this[symbols]
+        }
+
+        symbols++
+    }
+
+    return result
+}

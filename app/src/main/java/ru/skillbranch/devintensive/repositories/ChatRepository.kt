3a98fbdc7cf2b4cd70c5d5f89baa130e749eab9c
Nekobitlz <jokers.bit@gmail.com>
2019-09-03 22:49:50 +0300

    Make the main part of the recycler with single chats

--- app/src/main/java/ru/skillbranch/devintensive/repositories/ChatRepository.kt
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/ChatRepository.kt
@@ -1,5 +1,9 @@
 package ru.skillbranch.devintensive.repositories
 
+import ru.skillbranch.devintensive.models.Chat
+import ru.skillbranch.devintensive.utils.DataGenerator
+
 object ChatRepository {
+    fun loadChats(): List<Chat> = DataGenerator.generateChats(10)
 
 }

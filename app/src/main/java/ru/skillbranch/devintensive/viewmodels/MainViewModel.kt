c42eab3c180f39e55b4ceaca0fb8d918882fcb75
Nekobitlz <jokers.bit@gmail.com>
2019-09-06 13:01:30 +0300

    Implement search of users on MainActivity

--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
@@ -1,15 +1,16 @@
 package ru.skillbranch.devintensive.viewmodels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.ChatItem
 import ru.skillbranch.devintensive.repositories.ChatRepository
-import ru.skillbranch.devintensive.utils.DataGenerator
 
 class MainViewModel: ViewModel() {
 
+    private val query = mutableLiveData("")
     private val chatRepository = ChatRepository
     private val chats = Transformations.map(chatRepository.loadChats()) { chats ->
 
@@ -19,7 +20,20 @@ class MainViewModel: ViewModel() {
     }
 
     fun getChatData(): LiveData<List<ChatItem>> {
-        return chats
+        val result = MediatorLiveData<List<ChatItem>>()
+
+        val filterF = {
+            val queryStr = query.value!!
+            val chats = chats.value!!
+
+            result.value = if (queryStr.isEmpty()) chats
+            else chats.filter { it.title.contains(queryStr, true) }
+        }
+
+        result.addSource(chats) { filterF.invoke() }
+        result.addSource(query) { filterF.invoke() }
+
+        return result
     }
 
     fun addToArchive(chatId: String) {
@@ -34,5 +48,9 @@ class MainViewModel: ViewModel() {
 
         chat ?: return
         chatRepository.update(chat.copy(isArchived = false))
+    }
+
+    fun handleSearchQuery(text: String) {
+        query.value = text
     }
 }
